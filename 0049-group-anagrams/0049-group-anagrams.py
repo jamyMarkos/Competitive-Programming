cd7bd296981178a16e4429037607135f104188b2
jamyMarkos <103282607+jamyMarkos@users.noreply.github.com>
2022-12-28 23:54:16 +0300

    Time: 105 ms (87.13%), Space: 17.7 MB (65.46%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        dict_ = defaultdict(list)
+        
+        for str_ in strs:
+            temp = ''.join(sorted(str_))
+            dict_[temp].append(str_)
+            
+        return dict_.values()
+        
+                
+            
+                
+            
+            
+            
+            
+            
+        
