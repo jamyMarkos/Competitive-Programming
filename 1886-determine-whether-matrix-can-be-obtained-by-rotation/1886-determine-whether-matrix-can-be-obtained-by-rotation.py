cbc9bd5d38700d7a81ec9bf91b9299a8b161ab47
jamyMarkos <103282607+jamyMarkos@users.noreply.github.com>
2023-01-17 01:49:57 +0300

    Time: 42 ms (89.49%), Space: 13.9 MB (29.95%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,16 +1,30 @@
 class Solution:
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        if target == mat:
+        if mat == target:
             return True
-        for i in range(3):
-            for j in range(len(mat)):
-                for k in range(j+1, len(mat)):
-                    mat[j][k], mat[k][j] = mat[k][j], mat[j][k]
-                mat[j] = mat[j][::-1]
-            if target == mat:
-                return True
-        return False
-
+        for _ in range(3):
+            self.transpose(mat)
+            self.flip(mat)
             
+            if mat == target: 
+                return True
+            
+        return False
+            
+            
+    def transpose(self, mat):
+        rows, cols = len(mat), len(mat[0])
+        for row in range(rows):
+            for col in range(row+1, cols):
+                mat[row][col], mat[col][row] = mat[col][row], mat[row][col]
+        
+    def flip(self, mat):
+        for row in mat:
+            left, right = 0, len(row) - 1
+            while left < right:
+                row[left], row[right] = row[right], row[left]
+                left += 1
+                right -= 1
+        
         
         
