5ce293e94af37dbcb94d5fdf4c1164d85c359210
jamyMarkos <103282607+jamyMarkos@users.noreply.github.com>
2023-02-16 14:14:57 +0300

    Time: 38 ms (47.36%), Space: 13.9 MB (57.18%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,19 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        hash_ = dict(sorted(Counter(s).items(), key = lambda x: x[1], reverse=True))
         count = 0
-        firstOdd = True
-        for key in hash_.keys():
-            if hash_[key] % 2:
-                if firstOdd:
-                    count += hash_[key]
-                    firstOdd = False
-                    continue
-                count += (hash_[key] - 1)
+        odd = 0
+        for val in Counter(s).values():
+            if val % 2:
+                odd = 1
+                count += val - 1
             else:
-                count += hash_[key]
-                
-        return count
+                count += val  
+        return count + odd
             
         
         
