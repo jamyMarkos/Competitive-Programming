2c6ae0a96ddff24e7846a8ec98a24c2a0960daaa
Jay_ <103282607+jamyMarkos@users.noreply.github.com>
2023-03-07 08:47:06 +0300

    added subarray-sums-divisible-by-k.java

--- subarray-sums-divisible-by-k..java
+++ subarray-sums-divisible-by-k..java
@@ -1,11 +1,14 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        cur = 0
-        counter = Counter()
-        for num in nums:
-            cur = (cur + num) % k
-            counter[cur] += 1
-        res = 0
-        for e in counter:
-            res += counter[e]*(counter[e]-1)//2
-        return res + counter[0]
+        prefix=[0]
+        rem={}
+        count=0
+        for i in nums:
+            prefix.append(prefix[-1]+i)
+        for i in prefix:
+            if i%k in rem:
+                count+=rem[i%k]
+                rem[i%k]+=1
+            else:
+                rem[i%k]=1
+        return count
