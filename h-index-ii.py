6c64bd22b7d09f3db191fed4202a03878781e91e
Jay_ <103282607+jamyMarkos@users.noreply.github.com>
2023-03-22 16:54:09 +0300

    added h-index-ii.python

---
+++ h-index-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        def isPossible(n):
+            cnt = 0
+            for num in citations:
+                if num >= n:
+                    cnt += 1
+            return cnt >= n
+
+        l, r = -1, max(citations) + 1
+        while l+1 < r:
+            mid = (l + r)//2
+            if isPossible(mid):
+                l = mid
+            else:
+                r = mid
+        return l
