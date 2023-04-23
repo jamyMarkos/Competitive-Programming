49efb53e51f985f0e3851c03bbb782c75b009c86
Mintesnot Markos <103282607+jamyMarkos@users.noreply.github.com>
2023-04-23 15:52:00 +0300

    added find_complement.py

---
+++ find_complement.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        binary = bin(num)[2:]
+
+        flipped_binary = ''.join(['0' if bit == '1' else '1' for bit in binary])
+
+        if flipped_binary:
+            return int(flipped_binary, 2)
+        else:
+            return 1
+
+            
+        
