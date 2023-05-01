a7078160c65a0e4a88d347ccf56315f0024ab70f
Mintesnot Markos <103282607+jamyMarkos@users.noreply.github.com>
2023-05-01 12:03:29 +0300

    added open-the-lock.python

--- open-the-lock.py
+++ open-the-lock.py
@@ -1,47 +1,38 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        
-        def convert(val):
-            return tuple(map(int, [*val])) 
-
-        visited = set(list(map(convert, deadends)))
-        queue = deque([(0,0,0,0)])
-        target = convert(target)
-        turns = 1
-
-        if target == (0,0,0,0):
-            return 0
-        
-        if (0,0,0,0) in visited:
+        visit = set(deadends)
+        if '0000' in visit:
             return -1
+        if target == '0000':
+            return 0
 
-        while queue:
-            for i in range(len(queue)):
-                key = list(queue.popleft())
-
+        visit.add('0000')
+        q = deque(['0000'])
+        lvl = 0
+        while q:
+            lvl += 1
+            for _ in range(len(q)):
+                key = list(map(int, q.popleft()))
                 for i in range(4):
-                    key[i] = ((key[i]+1)%10)
-
-                    new_val = tuple(key)
-                    if new_val not in visited:
-                        visited.add(new_val)
-                        queue.append(new_val)
+                    key[i] = (key[i] + 1) % 10
+                    tt = ''.join(map(str,key))
+                    if tt not in visit:
+                        q.append(tt)
+                        visit.add(tt)
                     
-                    if new_val == target:
-                        return turns
-
-                    key[i] = ((key[i]-2)%10)
+                    if target == tt:
+                        return lvl
                     
-
-                    new_val = tuple(key)
-                    if new_val not in visited:
-                        visited.add(new_val)
-                        queue.append(new_val)
+                    key[i] = (key[i]-2) % 10
+                    tt = ''.join(map(str, key))
+                    if tt not in visit:
+                        q.append(tt)
+                        visit.add(tt)
                     
-                    if new_val == target:
-                        return turns
-                    key[i] = ((key[i]+1)%10)
+                    if target == tt:
+                        return lvl
 
-            turns += 1
+                    key[i] = (key[i]+1) % 10
+
 
         return -1
