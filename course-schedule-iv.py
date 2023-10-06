cc33d4f2cce57a5c8cbfecbda4f99a3bd2653f4f
Mintesnot Markos <103282607+jamyMarkos@users.noreply.github.com>
2023-10-06 09:33:39 +0300

    added course-schedule-iv.python

--- course-schedule-iv.py
+++ course-schedule-iv.py
@@ -1,30 +1,23 @@
 class Solution:
-    def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        dist = [[float('inf')] * numCourses for _ in range(numCourses)]
         
-        res = [set() for _ in range(n)]
-        indeg = [0] * (n)
-        graph = defaultdict(list)
-        q = deque()
-
-        for a, b in prerequisites:
-            graph[a].append(b)
-            indeg[b] += 1
-
-        for i in range(n):
-            if not indeg[i]:
-                q.append(i)
-
-        while q:
-            cur = q.popleft()
-            for nbr in graph[cur]:
-                res[nbr].add(cur)
-                for el in res[cur]:
-                    res[nbr].add(el)
-                indeg[nbr] -= 1
-                if not indeg[nbr]:
-                    q.append(nbr)
+        for i, j in prerequisites:
+            dist[i][j] = 1
+        
+        for i in range(numCourses):
+            dist[i][i] = 0
+            
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+                    
         ans = []
-        for u, v in queries:
-            ans.append(u in res[v])
-
+        for i, j in queries:
+            if dist[i][j] == float("inf"):
+                ans.append(False)
+            else:
+                ans.append(True)
         return ans
