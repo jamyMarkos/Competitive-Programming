66f6dfb51f21a378c2d42c9117d32a76d28e4f47
Mintesnot Markos <103282607+jamyMarkos@users.noreply.github.com>
2023-10-12 09:15:50 +0300

    added prefix-and-suffix-search.python

--- prefix-and-suffix-search.py
+++ prefix-and-suffix-search.py
@@ -1,23 +1,45 @@
-Trie = lambda: collections.defaultdict(Trie)
-WEIGHT = False
+class TrieNode:
+    
+    def __init__(self):
+        self.children = {}
+        self.index = 0
+        self.is_end = False
+
 
 class WordFilter:
-    def __init__(self, words: List[str]):
-        self.trie = Trie()
 
-        for weight, word in enumerate(words):
-            word += '#'
-            for i in range(len(word)):
-                cur = self.trie
-                cur[WEIGHT] = weight
-                for j in range(i, 2 * len(word) - 1):
-                    cur = cur[word[j % len(word)]]
-                    cur[WEIGHT] = weight
+    def __init__(self, words: List[str]):
+        self.root = TrieNode()
+        temp = self.root
+        for i,word in enumerate(words):
+            self.insert(word,i)
+    def insert(self, word: str,i : int) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.index = i
+        curr.is_end = True
 
     def f(self, pref: str, suff: str) -> int:
-        cur = self.trie
-        for letter in suff + '#' + pref:
-            if letter not in cur:
+        ind = -1
+        temp = self.root
+        for i in pref:
+            if i not in temp.children:
                 return -1
-            cur = cur[letter]
-        return cur[WEIGHT]
+            temp = temp.children[i]
+        def dfs(node,w):
+            nonlocal ind
+            if node.is_end:
+                if w[len(w) - len(suff):] == suff:
+                    ind = max(ind,node.index)
+            for i in node.children:
+                dfs(node.children[i],w + i)
+
+        dfs(temp,pref)
+        return ind
+
+# Your WordFilter object will be instantiated and called as such:
+# obj = WordFilter(words)
+# param_1 = obj.f(pref,suff)
