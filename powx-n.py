d2e7cb489908c77a25f5357451a897ac9c679ea3
Mintesnot Markos <103282607+jamyMarkos@users.noreply.github.com>
2023-10-18 13:37:08 +0300

    added powx-n.python

--- powx-n.py
+++ powx-n.py
@@ -5,7 +5,8 @@ class Solution:
             if not n:
                 return 1
             if not n % 2:
-                return func(x * x, n // 2)
+                left = func(x, n // 2)
+                return left * left
             return x * func(x, n - 1)
 
         return func(x, abs(n)) if n > 0 else 1 / func(x, abs(n))
